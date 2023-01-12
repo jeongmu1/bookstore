f408ae48bd663d550cd26269f7e29ea035300831
Kang <Kang815595@gmail.com>
2023-01-12 22:00:07 +0900

    주문번호로 DB 조회하는 메소드 추가

--- src/main/java/books/order/repository/OrderRepository.java
+++ src/main/java/books/order/repository/OrderRepository.java
@@ -2,11 +2,16 @@ package books.order.repository;
 
 import books.order.domain.ProductOrder;
 import books.user.domain.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface OrderRepository extends CrudRepository<ProductOrder, Long> {
     public Optional<Set<ProductOrder>> findAllByUser(User user);
+
+    public Optional<ProductOrder> findProductOrderByOrderUuid(String orderUuid);
+    public Optional<List<ProductOrder>> findAllByUserOrderByCreateTimeDesc(User user, Pageable pageable);
 }
