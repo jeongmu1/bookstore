9f642c70a4352b03650378eec794e0b8a9539e13
Kang <Kang815595@gmail.com>
2023-01-12 21:55:11 +0900

    yaml 에 정의되어 있는 구성요소를 받아오는 클래스 생성

---
+++ src/main/java/books/common/PageSizeProps.java
@@ -0,0 +1,13 @@
+package books.common;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "bookstore.page-size")
+@Data
+public class PageSizeProps {
+    private int userOrders;
+    private int pointHistories;
+}
