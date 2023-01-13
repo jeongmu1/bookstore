def7bd29e6ea938f8ee8b9e4878e16268f1f7758
Kang <Kang815595@gmail.com>
2023-01-13 21:43:56 +0900

    Java 11 로 변경

--- src/main/java/books/home/repository/ProductBookRepository.java
+++
@@ -1,7 +0,0 @@
-package books.home.repository;
-
-import books.book.domain.ProductBook;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductBookRepository extends CrudRepository<ProductBook, Long> {
-}
