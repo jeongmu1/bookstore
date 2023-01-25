c077919763c8b427d140295a7dc3c2ad1f37e7fa
Kang <Kang815595@gmail.com>
2023-01-25 20:35:22 +0900

    feature: 화면 상단 장바구니 정보 추가, 검색 추가

--- src/main/java/books/common/ControllerAspect.java
+++ src/main/java/books/common/ControllerAspect.java
@@ -1,5 +1,7 @@
 package books.common;
 
+import books.product.common.CartItemDto;
+import books.product.service.CartService;
 import books.user.service.UserService;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -7,25 +9,36 @@ import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
 import java.security.Principal;
+import java.util.List;
 
 @Aspect
 @Component
 public class ControllerAspect {
     private final UserService userService;
+    private final CartService cartService;
 
-    public ControllerAspect(UserService userService) {
+    public ControllerAspect(UserService userService, CartService cartService) {
         this.userService = userService;
+        this.cartService = cartService;
     }
 
-    @Before(value = "execution(* books.*.controller.*Controller.show*(..)) && args(model, principal)", argNames = "model,principal")
-    public void setLoginDetails(Object model, Object principal) {
+    @Before(value = "execution(* books.*.controller.*Controller.show*(..)) && args(model, principal, ..)", argNames = "model,principal")
+    public void initializeDetails(Object model, Object principal) {
         Model m = (Model) model;
         Principal p = (Principal) principal;
 
+
         if (p == null) {
             m.addAttribute("member", "Guest");
+            m.addAttribute("cartTotal", "0");
+            m.addAttribute("cartTotalPrice", "0");
+
         } else {
             userService.loadUserByUsername(p.getName()).ifPresent(user -> m.addAttribute("member", user.getName()));
+
+            List<CartItemDto> cart = cartService.findCartByUser(p);
+            m.addAttribute("cartTotal", cart.size());
+            m.addAttribute("cartTotalPrice", cartService.getTotalPrice(cart).get("totalPrice"));
         }
     }
 }
