6c7fc680d7721fbcb11d3d6d735d7a8d82e631f0
Kang <Kang815595@gmail.com>
2023-01-26 18:36:54 +0900

    delete: 유저 DB 접근 메소드 필요 시 구현 예정

--- src/main/java/books/user/service/UserService.java
+++ src/main/java/books/user/service/UserService.java
@@ -1,32 +1,19 @@
 package books.user.service;
 
-import books.order.domain.ProductOrder;
+import books.user.common.PointHistoryDto;
 import books.user.common.RegistrationForm;
-import books.user.domain.User;
-import books.user.domain.UserAddress;
-import books.user.domain.UserCreditCard;
-import books.user.domain.UserPointHistory;
-import org.springframework.ui.Model;
+import books.user.common.UserDto;
+import books.user.domain.*;
 
+import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 public interface UserService {
-    User getUserInfo(User user);
-
-    List<ProductOrder> getUserOrdersPage(User user, int page);
-
-    ProductOrder getOrderDetail(String orderUuid);
-
-    List<UserPointHistory> getUserPointHistoriesPage(User user);
-
-    Set<UserAddress> getUserAddresses(User user);
-
-    Set<UserCreditCard> getUserCreditCards(User user);
+    UserDto findUserInfo(Principal principal);
 
     void processRegistration(RegistrationForm form);
 
-    Optional<User> loadUserByUsername(String username);
+    User loadUserByUsername(String username);
 
+    List<PointHistoryDto> findUserPointHistory(Principal principal);
 }
