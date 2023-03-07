ed19dc9c591b6f2acf1f3703ec827c69ed9c69c4
Kang <Kang815595@gmail.com>
2023-03-07 21:46:29 +0900

    refactor: cartService 로 직접 Principal 객체를 주입하지 못하도록 변경

--- src/main/java/books/product/service/CartService.java
+++ src/main/java/books/product/service/CartService.java
@@ -8,15 +8,15 @@ import java.util.List;
 import java.util.Map;
 
 public interface CartService {
-    List<CartItemDto> findCartByUser(Principal principal);
+    List<CartItemDto> findCartByUser(String username);
 
-    void addProductInCart(Principal principal, int amount, long itemId);
+    void addProductInCart(String username, int amount, long itemId);
 
     Map<String, Object> getTotalPrice(List<CartItemDto> cart);
 
-    void modifyProductCount(Principal principal, String amounts);
+    void modifyProductCount(String username, String amounts);
 
-    void deleteProduct(Principal principal, long itemId);
+    void deleteProduct(String username, long itemId);
 
-    void deleteAll(Principal principal);
+    void deleteAll(String username);
 }
